d39805185bc3f5520253ba1ed79b0e3d67d635aa
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-22 17:18:01 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,12 +23,6 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     profile_hash = {}
-    # profile_hash[:twitter] = []
-    # profile_hash[:linkedin] = []
-    # profile_hash[:github] = []
-    # profile_hash[:blog] = []
-    # profile_hash[:profile_quote] = []
-    # profile_hash[:bio] = []
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
     main_wrapper = doc.css(".main-wrapper")
@@ -60,6 +54,7 @@ class Scraper
     main_wrapper.each do |x|
       if x.css(".vitals-container .social-icon-container a")[3]["href"].include?("http")
         profile_hash[:blog] = x.css(".vitals-container .social-icon-container a")[3]["href"]
+        binding.pry
       end
       profile_hash[:profile_quote] = x.css(".vitals-container .vitals-text-container .profile-quote").text
       profile_hash[:bio] = x.css(".details-container .description-holder p").text
